6c6f4877eb2c45979d08e064734c579cb42979dc
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-11-03 16:08:09 -0800

    Create meme command

---
+++ commands/meme.js
@@ -0,0 +1,14 @@
+const { Attachment } = require('discord.js');
+const axios = require('axios');
+
+meme = async message => {
+	// fetch meme from api
+	const { data } = await axios.get('https://meme-api.herokuapp.com/gimme');
+
+	// create message message attachment
+	const attachment = new Attachment(data.url);
+
+	return message.channel.send(attachment);
+};
+
+module.exports = meme;
