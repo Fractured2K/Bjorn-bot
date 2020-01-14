03fe8ade6dba86d975cd6a23ab0c8b1cc422788d
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2020-01-14 03:33:11 -0800

    Refactor 8ball to respond with an embeded message

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,15 +1,25 @@
+const reponses = require("../helpers/8ballReponses");
+const parseMessage = require("../utils/parseMessage");
+
 /*
  * Returns random 8ball response
  */
 
-const reponses = require('../helpers/8ballReponses');
-
 eightBall = message => {
-	// generate random number between 1-20
-	const guess = Math.floor(Math.random() * Math.floor(20) + 1);
+  console.log(message);
+  // generate random number between 1-20
+  const guess = Math.floor(Math.random() * Math.floor(20) + 1);
 
-	// return reponse from eight ball reponses array
-	return message.channel.send(reponses[guess]);
+  // return reponse from eight ball reponses array
+  return message.channel.send({
+    embed: {
+      color: 3447003,
+      title: `${message.author.username} asked 8 ball, ${parseMessage(
+        message
+      )}?`,
+      description: reponses[guess]
+    }
+  });
 };
 
 module.exports = eightBall;
