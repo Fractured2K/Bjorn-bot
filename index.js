88f9b1d7c1d7419874163fbbf6ce5af399a320d6
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2020-01-14 03:34:28 -0800

    Add bot connnection checks

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const Discord = require('discord.js');
-const parseMessage = require('./utils/parseMessage');
+require("dotenv").config();
+const Discord = require("discord.js");
+const parseCommand = require("./utils/parseCommand");
 
 // Create discord client instance
 const client = new Discord.Client();
@@ -8,12 +8,22 @@ const client = new Discord.Client();
 // Login into discord cline
 client.login(process.env.DISCORD_CLIENT_SECRET);
 
+// Confirm sucessful login
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// Set status of bot on successful connection
+client.on("ready", () => {
+  client.user.setStatus(process.env.BOT_STATUS);
+});
+
 // Listen for message event
-client.on('message', message => {
-	try {
-		// Parse message
-		parseMessage(message);
-	} catch (error) {
-		throw new Error(error);
-	}
+client.on("message", message => {
+  try {
+    // Parse message
+    parseCommand(message, client);
+  } catch (error) {
+    throw new Error(error);
+  }
 });
