ff1f57b1be4f16e859c1fa0229e615466c6014e0
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2020-01-14 04:18:23 -0800

    8ball validation

--- commands/8ball.js
+++ commands/8ball.js
@@ -6,17 +6,19 @@ const parseMessage = require("../utils/parseMessage");
  */
 
 eightBall = message => {
-  console.log(message);
   // generate random number between 1-20
   const guess = Math.floor(Math.random() * Math.floor(20) + 1);
+
+  // parse message
+  const parsedMessage = parseMessage(message)
+
+  if (!parsedMessage) return message.channel.send("Please provide a question to the eight ball :<")
 
   // return reponse from eight ball reponses array
   return message.channel.send({
     embed: {
       color: 3447003,
-      title: `${message.author.username} asked 8 ball, ${parseMessage(
-        message
-      )}?`,
+      title: `${message.author.username} asked 8 ball, ${parsedMessage}?`,
       description: reponses[guess]
     }
   });
