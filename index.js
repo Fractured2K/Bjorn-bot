2902d27c16dd44ee560934f5b18c30b7ea023c7c
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2020-01-15 02:21:30 -0800

    Add message validation

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 require("dotenv").config();
-const Discord = require("discord.js");
-const parseCommand = require("./utils/parseCommand");
+const Client = require('./client/Client');
+const executeCommand = require("./utils/executeCommand");
 
-// Create discord client instance
-const client = new Discord.Client();
 
-// Login into discord cline
+const client = new Client();
 client.login(process.env.DISCORD_CLIENT_SECRET);
 
-// Log sucessful login
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
-});
-
-// Set status of bot
-client.on("ready", () => {
   client.user.setActivity(process.env.BOT_STATUS);
 });
 
-
-// Listen for message event
 client.on("message", message => {
   try {
-    // Parse message
-    parseCommand(message, client);
+	if (message.author.bot) return;
+	if (!message.content.startsWith(process.env.COMMADN_PREFIX)) return;
+    executeCommand(message);
   } catch (error) {
     throw new Error(error);
   }
