200eaaac123effb7db601659001b4ac405dcba90
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2020-01-16 04:26:46 -0800

    Create pause command

---
+++ commands/pause.js
@@ -0,0 +1,32 @@
+pause = message => {
+  if (!message.member.voiceChannel) {
+    return message.channel.send("Please join a voice channel first");
+  }
+
+  const server = message.client.queue.get(message.guild.id);
+
+  if (!server) {
+    return message.channel.send("No songs currently playing.");
+  }
+
+  if (server.playing) {
+    server.playing = false;
+    // pause song
+    server.connection.dispatcher.pause();
+  } else {
+    server.playing = true;
+    // resume song
+    server.connection.dispatcher.resume();
+  }
+
+  return message.channel.send({
+    embed: {
+      color: 10181046,
+      description: `:play_pause: [${server.songs[0].title}](${
+        server.songs[0].video_url
+      }) has been ${server.playing ? "**resumed**" : "**paused**"}`
+    }
+  });
+};
+
+module.exports = pause;
