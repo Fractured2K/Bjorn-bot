ebcec147e7b995e68ad4935498cf123ee68c9907
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2020-01-15 22:44:55 -0800

    Url validation

--- commands/play.js
+++ commands/play.js
@@ -2,19 +2,24 @@ const ytdl = require("ytdl-core-discord");
 const parseMessage = require("../utils/parseMessage");
 
 play = async message => {
-  const queue = message.client.queue;
+  const server = message.client.queue;
   const voiceChannel = message.member.voiceChannel;
+  const url = parseMessage(message);
 
   if (!voiceChannel) {
     return message.channel.send("Please join a voice channel first");
   }
 
+  if (!url) {
+    return message.channel.send("Please provide a url or text to play a song");
+  }
+
   // join voice channel and establish a connection
   const connection = await message.member.voiceChannel.join();
 
-  // check to see if server queue exists
-  if (!queue.get(message.guild.id)) {
-    queue.set(message.guild.id, {
+  // check to see if server connection exists
+  if (!server.get(message.guild.id)) {
+    server.set(message.guild.id, {
       textChannel: message.channel,
       voiceChannel,
       connection,
@@ -24,39 +29,39 @@ play = async message => {
   }
 
   // add song to queue
-  queue.get(message.guild.id).songs.push(parseMessage(message));
+  server.get(message.guild.id).songs.push(url);
 
-  console.log(queue.get(message.guild.id).songs);
-  if (queue.get(message.guild.id).playing) {
+  if (server.get(message.guild.id).playing) {
     return message.channel.send("Song added to queue");
   }
 
   // play song
-  _playSong(message, queue);
+  _playSong(message, server);
 };
 
-async function _playSong(message, queue) {
-  queue.get(message.guild.id).playing = true;
+async function _playSong(message, server) {
+  const queue = server.get(message.guild.id);
+
+  // set queue to playing
+  queue.playing = true;
 
   // stream song
-  const dispatcher = await queue
-    .get(message.guild.id)
-    .connection.playOpusStream(
-      await ytdl(queue.get(message.guild.id).songs[0])
-    );
+  const dispatcher = await queue.connection.playOpusStream(
+    await ytdl(queue.songs[0])
+  );
 
   // the song has finished
   dispatcher.on("end", () => {
     // remove song from queue
-    queue.get(message.guild.id).songs.shift();
+    queue.songs.shift();
 
     // play next song
-    if (queue.get(message.guild.id).songs.length > 0) {
-      return _playSong(message, queue);
+    if (queue.songs.length > 0) {
+      return _playSong(message, server);
     }
 
-    // remove server from queue
-    queue.delete(message.guild.id);
+    // remove server connection
+    server.delete(message.guild.id);
 
     // leave voice channel
     message.member.voiceChannel.leave();
