21832ee7aba121f19171d37365b202a2e824a92e
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2020-01-16 04:27:30 -0800

    Create formatSong helper

---
+++ helpers/formatSong.js
@@ -0,0 +1,29 @@
+const ytdl = require("ytdl-core-discord");
+
+
+
+/*@
+* Accepts a youtube url and returns a formatted song object
+* @param String url
+* @param {Message}
+*/
+
+formatSong = async (url, message) => {
+	// query youtube video meta data
+	const songInfo = await ytdl.getInfo(url);
+
+	// select largest thumbnail from thumbnails array
+	const songThumbnails = songInfo.player_response.videoDetails.thumbnail.thumbnails;
+
+	return {
+	  uploader: songInfo.author.name,
+	  uploader_url:`https://www.youtube.com/channel/${songInfo.author.id}`,
+	  thumbnail: songThumbnails[songThumbnails.length - 1].url,
+	  title: songInfo.title,
+	  video_url: songInfo.video_url,
+	  length: songInfo.length_seconds,
+	  addedBy: message.author.toString()
+	};
+}
+
+module.exports = formatSong;
