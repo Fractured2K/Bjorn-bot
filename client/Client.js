fb3abbe2a1ae760681af719057b267f20a6e685a
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2020-01-17 13:35:59 -0800

    Add commands property to client

--- client/Client.js
+++ client/Client.js
@@ -3,6 +3,7 @@ const { Client } = require("discord.js");
 module.exports = class extends Client {
   constructor() {
     super();
+    this.commands = new Map();
     this.queue = new Map();
   }
 };
