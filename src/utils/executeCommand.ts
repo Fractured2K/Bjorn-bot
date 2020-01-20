252cd7a87d7f169d822d5314455563dab7a669cf
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2020-01-20 05:12:20 -0800

    Update tsconfig

--- src/utils/executeCommand.ts
+++ src/utils/executeCommand.ts
@@ -3,23 +3,27 @@ import { Message } from "discord.js";
 
 export const executeCommand = async (message: Message) => {
   // load commands
-  if ((message.client as any).commands.size == 0) {
-    await _loadCommands(message);
+  try {
+    if ((message.client as any).commands.size == 0) {
+      await _loadCommands(message);
+    }
+
+    // parse command form message
+    const commandArg = message.content
+      .toLowerCase()
+      .slice(1)
+      .split(" ")[0];
+
+    const command = (message.client as any).commands.get(commandArg);
+
+    if (command) {
+      return command[Object.keys(command)[0]](message);
+    } else {
+      return message.channel.send("Sorry, but that command doesn't exist");
+    }
+  } catch (error) {
+    console.log(error);
   }
-
-  // parse command form message
-  const commandArg = message.content
-    .toLowerCase()
-    .slice(1)
-    .split(" ")[0];
-
-  const command = (message.client as any).commands.get(commandArg);
-
-  if (command) {
-    return command[Object.keys(command)[0]](message);
-  }
-
-  return message.channel.send("Sorry, but that command doesn't exist");
 };
 
 async function _loadCommands(message: Message) {
